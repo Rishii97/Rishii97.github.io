94679f16a4caf046ee18cf6408360ada632efe87
Rishi Pandey <pandeyrishi760@gmail.com>
2024-10-02 20:02:19 -0400

    main change 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,14 @@
     });
   });
 
+  // The additional script for toggling resume content
+document.querySelectorAll('.toggle').forEach(function(toggle) {
+  toggle.addEventListener('click', function() {
+    var content = this.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
+});
+
   /**
    * Preloader
    */
